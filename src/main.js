8118f6f0bc42985e47091eb504d3f689a12f20c7
MilesAndersonArt <milesandersonart@gmail.com>
2023-04-25 15:44:37 -0700

    modified:   src/main.js

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
+// Miles Anderson
+// Title: Red Skies
+// Time Taken:
+// 
+// Mods Used:
+// Create 4 new explosion sound effects and randomize which one plays on impact (10)
+// Display the time remaining (in seconds) on the screen (10)
+// Using a texture atlas, create a new animated sprite for the Spaceship enemies (10)
+// Create a new title screen (e.g., new artwork, typography, layout) (10)
+// 
+// Sources:
+// 
+
 let config = {
     type: Phaser.CANVAS,
     width: 640,
