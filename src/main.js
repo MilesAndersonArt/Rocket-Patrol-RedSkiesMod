494680d263e715ff39837dacdf92c6896f21e974
MilesAndersonArt <milesandersonart@gmail.com>
2023-05-12 19:03:46 -0700

    Fixed main.js criteria score

--- src/main.js
+++ src/main.js
@@ -3,17 +3,17 @@
     Time Taken: 14 hours 
     
     Mods Used:
-    Track a high score that persists across scenes and display it in the UI (5) +
-    Implement the speed increase that happens after 30 seconds in the original game (5)
-    Create a new scrolling tile sprite for the background (5) +
-    Create 4 new explosion sound effects and randomize which one plays on impact (10) +
-    Display the time remaining (in seconds) on the screen (10) +
-    Using a texture atlas, create a new animated sprite for the Spaceship enemies (10) +
-    Create a new title screen (e.g., new artwork, typography, layout) (10)
-    Implement parallax scrolling for the background (10) +
-    Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15) +
-    Implement mouse control for player movement and mouse click to fire (15) +
-    Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15) +
+    + Track a high score that persists across scenes and display it in the UI (5)
+    + Implement the speed increase that happens after 30 seconds in the original game (5)
+    + Create a new scrolling tile sprite for the background (5)
+    + Create 4 new explosion sound effects and randomize which one plays on impact (10)
+    + Display the time remaining (in seconds) on the screen (10)
+    + Using a texture atlas, create a new animated sprite for the Spaceship enemies (10)
+    + Create a new title screen (e.g., new artwork, typography, layout) (10)
+    + Implement parallax scrolling for the background (10)
+    + Create a new enemy Spaceship type (w/ new artwork) that's smaller, moves faster, and is worth more points (15)
+    + Implement mouse control for player movement and mouse click to fire (15)
+    + Implement a new timing/scoring mechanism that adds time to the clock for successful hits (15)
  
     Sources:
     - 'enemydeath' sound effects recorded by me through Voice Memos app
